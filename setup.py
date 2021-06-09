19fa698a54536e924aad4ca28ca08a1bc3ec5e99
niravthar <78552575+niravthar@users.noreply.github.com>
2021-06-08 20:46:56 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+# setup.py
+import setuptools
+REQUIRED_PACKAGES = [
+    'google',
+    'google-cloud-storage',
+    'google-cloud-resource-manager',
+    'apache-beam',
+    'apache-beam[GCP]'
+]
+
+setuptools.setup(
+    name='enrichlogs',
+    version='0.0.1',
+    description='Enrich Logs',
+    install_requires=REQUIRED_PACKAGES,
+    packages=setuptools.find_packages(),
+)
